8dbc915d3a094e06783cc6a3141850fec5c8fceb
Ali Arab <aliarab812@yahoo.com>
2021-03-06 12:51:21 -0600

    updating html and javascript side of the code

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+// list out all elements
+
+var timeEl = document.getElementById("time-left");
+var startQuiz = document.getElementById("begin-quiz");
+var buttonChoice = document.getElementById("options");
+
+
+
+
